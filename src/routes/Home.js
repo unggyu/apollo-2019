634a7424e8969535bedda5b8a35b6d58b4574b04
Choi-UngGyu <cdr982116@gmail.com>
2020-12-03 13:23:06 +0900

    1.3 Apollo Cache and Styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,46 +5,56 @@ import Movie from '../components/Movie';
 import styled from 'styled-components';
 
 const GET_MOVIES = gql`
-{
-    movies {
-        id
-        medium_cover_image
+    {
+        movies {
+            id
+            medium_cover_image
+        }
     }
-}`;
+`;
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
 `;
 
 const Header = styled.header`
-  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
-  height: 45vh;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    height: 45vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
 `;
 
 const Title = styled.h1`
-  font-size: 60px;
-  font-weight: 600;
-  margin-bottom: 20px;
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom: 20px;
 `;
 
 const Subtitle = styled.h3`
-  font-size: 35px;
+    font-size: 35px;
 `;
 
 const Loading = styled.div`
-  font-size: 18px;
-  opacity: 0.5;
-  font-weight: 500;
-  margin-top: 10px;
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`;
+
+const Movies = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    width: 60%;
+    position: relative;
+    top: -50px;
 `;
 
 export default () => {
@@ -56,9 +66,13 @@ export default () => {
                 <Subtitle>I love GraphQL</Subtitle>
             </Header>
             {loading && <Loading>Loading...</Loading>}
-            {!loading &&
-              data.movies &&
-              data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+            {!loading && data.movies && (
+                <Movies>
+                    {data.movies.map(m => (
+                        <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+                    ))}
+                </Movies>
+            )}
         </Container>
     );
 }
