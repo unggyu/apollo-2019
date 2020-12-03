e8d366976e57606ba3c5f9b73e84a2bbf9eb56d1
Choi-UngGyu <cdr982116@gmail.com>
2020-12-03 11:06:18 +0900

    0.3 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Detail from '../routes/Detail';
+import Home from '../routes/Home';
 
 function App() {
   return (
-    <div className="App"></div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
   );
 }
 
