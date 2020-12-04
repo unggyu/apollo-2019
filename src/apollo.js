b8ff44d2c033110c4db15ab197b124e8f76de4aa
Choi-UngGyu <cdr982116@gmail.com>
2020-12-04 18:01:37 +0900

    what?

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000",
+    uri: "https://ung-movieql.herokuapp.com/",
     cache: new InMemoryCache(),
     resolvers: {
         Movie: {
